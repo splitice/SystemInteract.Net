8bd80eeb2d0754e9a5959cb3a0fc167883ff0485
splitice <mat999@gmail.com>
2014-01-26 19:45:08 +1030

    fix assemblyinfo

--- SystemInteract.Remote/Properties/AssemblyInfo.cs
+++ SystemInteract.Remote/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("IPTables.Net.Remote")]
+[assembly: AssemblyTitle("SystemInteract.Remote")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("IPTables.Net.Remote")]
+[assembly: AssemblyProduct("SystemInteract.Remote")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
