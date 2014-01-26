fc64f289aad661cd5e6db8b91e719196eba7b922
splitice <mat999@gmail.com>
2014-01-26 19:39:31 +1030

    initial code commit

---
+++ SystemInteract.Local/LocalFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SystemInteract.Local
+{
+    class LocalFactory
+    {
+        public ISystemProcess StartProcess(String command, String arguments)
+        {
+            return Local.LocalProcess.Start(new ProcessStartInfo(command, arguments));
+        }
+    }
+}
---
+++ SystemInteract.Remote/SshFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using Renci.SshNet;
+
+namespace SystemInteract.Remote.System
+{
+    class SshFactory
+    {
+        private readonly SshClient _connection;
+
+        public SshFactory(String host, String username, String password)
+        {
+            _connection = new SshClient(host, username, password);
+            _connection.Connect();
+        }
+        public ISystemProcess StartProcess(String command, String arguments)
+        {
+            return Remote.SshProcess.Start(new ProcessStartInfo(command, arguments), _connection);
+        }
+    }
+}
