8d841e6bcf5e0f77a09103b1405d6e0de337ca43
splitice <mat999@gmail.com>
2016-03-09 09:31:01 +1100

    Add ProcessHelper

---
+++ SystemInteract/ProcessHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace SystemInteract
+{
+    public class ProcessHelper
+    {
+        public static void ReadToEnd(ISystemProcess process, out String output, out String error)
+        {
+            String toutput = "";
+            String terror = "";
+
+            DataReceivedEventHandler errorEvent = (a, b) => terror += b.Data;
+            DataReceivedEventHandler outEvent = (a, b) => toutput += b.Data;
+
+            process.ErrorDataReceived += errorEvent;
+            process.OutputDataReceived += outEvent;
+
+            process.BeginErrorReadLine();
+            process.BeginOutputReadLine();
+
+            process.WaitForExit();
+
+            output = toutput;
+            error = terror;
+        }
+
+        public static void WaitForExit(ISystemProcess process)
+        {
+            String temp;
+
+            ReadToEnd(process, out temp, out temp);
+        }
+    }
+}
