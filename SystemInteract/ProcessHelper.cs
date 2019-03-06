46bc87ba47a9c5c94c9cc1ee97b081b6d084667c
Mathew Heard <splitice@users.noreply.github.com>
2019-03-06 19:11:28 +1100

    add callback method

--- SystemInteract/ProcessHelper.cs
+++ SystemInteract/ProcessHelper.cs
@@ -9,21 +9,21 @@ namespace SystemInteract
     public class ProcessHelper
     {
         private const int DefaultTimeout = 120;
-        public static void ReadToEnd(ISystemProcess process, out String output, out String error, int timeout = DefaultTimeout)
+
+        public static void ReadToEnd(ISystemProcess process, Action<String> output, Action<String> error,
+            int timeout = DefaultTimeout)
         {
-            StringBuilder toutput = new StringBuilder();
-            StringBuilder terror = new StringBuilder();
             DataReceivedEventHandler errorEvent = null, outEvent = null;
 
             if (process.StartInfo.RedirectStandardError)
             {
-                errorEvent = (a, b) => terror.AppendLine(b.Data);
+                errorEvent = (a, b) => error(b.Data);
                 process.ErrorDataReceived += errorEvent;
                 process.BeginErrorReadLine();
             }
             if (process.StartInfo.RedirectStandardOutput)
             {
-                outEvent = (a, b) => toutput.AppendLine(b.Data);
+                outEvent = (a, b) => output(b.Data);
                 process.OutputDataReceived += outEvent;
                 process.BeginOutputReadLine();
             }
@@ -33,9 +33,6 @@ namespace SystemInteract
                 throw new TimeoutException(String.Format("Timeout. Process did not complete executing within {0} seconds", timeout));
             }
 
-            output = toutput.ToString();
-            error = terror.ToString();
-
             if (errorEvent != null)
             {
                 process.ErrorDataReceived -= errorEvent;
@@ -44,6 +41,18 @@ namespace SystemInteract
             {
                 process.OutputDataReceived -= outEvent;
             }
+        }
+
+
+        public static void ReadToEnd(ISystemProcess process, out String output, out String error, int timeout = DefaultTimeout)
+        {
+            StringBuilder toutput = new StringBuilder();
+            StringBuilder terror = new StringBuilder();
+
+            ReadToEnd(process, a=>toutput.AppendLine(a), a=>terror.AppendLine(a));
+
+            output = toutput.ToString();
+            error = terror.ToString();
         }
 
         public static void WaitForExit(ISystemProcess process)
