0cb3dc33eecbdd48464d90475142c5a3f5ed80b5
Mathew Heard <splitice@users.noreply.github.com>
2019-10-19 18:34:22 +1100

    long test case

--- SystemInteract.Tests/Class1.cs
+++ SystemInteract.Tests/Class1.cs
@@ -11,13 +11,35 @@ namespace SystemInteract.Tests
     [TestFixture]
     public class Class1
     {
+        public static bool IsLinux
+        {
+            get
+            {
+                int p = (int)Environment.OSVersion.Platform;
+                return (p == 4) || (p == 6) || (p == 128);
+            }
+        }
+
         [TestCase]
         public void Test1()
         {
+            if (IsLinux) return;
             LocalFactory lf = new LocalFactory();
             var process = lf.StartProcess("netstat", "/a /n");
             String output, error;
             ProcessHelper.ReadToEnd(process, out output, out error);
         }
+
+        [TestCase]
+        public void TestLong()
+        {
+            if (!IsLinux) return;
+            LocalFactory lf = new LocalFactory();
+            var process = lf.StartProcess(@"awk", "'{for(i=1;i<1000;i++)print \"test\"}' /proc/loadavg");
+            String output, error;
+            ProcessHelper.ReadToEnd(process, out output, out error);
+        }
+
+        
     }
 }
