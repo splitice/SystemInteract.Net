1671d82983eef01d4bbd3ed2cf61067cf2fdeea1
Mathew Heard <splitice@users.noreply.github.com>
2020-10-28 21:37:10 +1100

    ...

--- nuget-upload.sh
+++ nuget-upload.sh
@@ -8,8 +8,8 @@ cd $P
 nuget update -self
 chmod 0777 /tmp/NuGetScratch -R
 
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-    nuget pack *.nuspec -Prop Configuration=Release -BasePath $P
+if [ "${CIRCLE_PULL_REQUEST}" = "" ]; then
+    dotnet  pack *.nuspec -Prop Configuration=Release -BasePath $P
 
-    nuget push *.nupkg -ApiKey $NUGET_API -Source https://www.nuget.org/api/v2/package
+    dotnet push *.nupkg -ApiKey $NUGET_API -Source https://www.nuget.org/api/v2/package
 fi
