1f2ebb2fe2d356c65e8b2fcf635477c875443ceb
Mathew Heard <splitice@users.noreply.github.com>
2020-10-28 22:01:00 +1100

    ...

--- nuget-upload.sh
+++ nuget-upload.sh
@@ -7,6 +7,6 @@ cd $P
 
 if [ "${CIRCLE_PULL_REQUEST}" = "" ]; then
     #dotnet pack --configuration Release  *.csproj
-
+    cat *.nuspec
     dotnet nuget push *.nuspec --api-key $NUGET_API -s https://www.nuget.org/api/v2/package
 fi
