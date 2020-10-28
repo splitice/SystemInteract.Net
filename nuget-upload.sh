966ac64b2691fde1ed676b2aa891f93c9fddc384
Mathew Heard <splitice@users.noreply.github.com>
2020-10-28 21:32:56 +1100

    ...

---
+++ nuget-upload.sh
@@ -0,0 +1,15 @@
+set -e
+
+DIR=~/repo
+P=$DIR/../$1
+
+cd $P
+
+nuget update -self
+chmod 0777 /tmp/NuGetScratch -R
+
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+    nuget pack *.nuspec -Prop Configuration=Release -BasePath $P
+
+    nuget push *.nupkg -ApiKey $NUGET_API -Source https://www.nuget.org/api/v2/package
+fi
